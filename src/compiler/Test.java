f71bbb2a07a1180d536aa7d85d868876493e128a
michele_bachetti <michele.bachetti@studio.unibo.it>
2024-04-09 18:12:43 +0200

    Feat: tested quicksort for oop extension

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -10,7 +10,7 @@ import svm.*;
 public class Test {
     public static void main(String[] args) throws Exception {
    			
-    	String fileName = "prova.fool";
+    	String fileName = "quicksort.fool";
 
     	CharStream chars = CharStreams.fromFileName(fileName);
     	FOOLLexer lexer = new FOOLLexer(chars);
@@ -23,9 +23,9 @@ public class Test {
     		parser.getNumberOfSyntaxErrors()+" syntax errors.\n");
 
     	System.out.println("Generating AST.");
-    	ASTGenerationSTVisitor visitor = new ASTGenerationSTVisitor(); // use true to visualize the ST
+    	ASTGenerationSTVisitor visitor = new ASTGenerationSTVisitor(true); // use true to visualize the ST
     	Node ast = visitor.visit(st);
-    	System.out.println("");
+    	System.out.println();
 
     	System.out.println("Enriching AST via symbol table.");
     	SymbolTableASTVisitor symtableVisitor = new SymbolTableASTVisitor();
@@ -34,7 +34,7 @@ public class Test {
 
     	System.out.println("Visualizing Enriched AST.");
     	new PrintEASTVisitor().visit(ast);
-    	System.out.println("");
+    	System.out.println();
 
     	System.out.println("Checking Types.");
     	try {
@@ -42,24 +42,24 @@ public class Test {
     		TypeNode mainType = typeCheckVisitor.visit(ast);
     		System.out.print("Type of main program expression is: ");
     		new PrintEASTVisitor().visit(mainType);
-    	} catch (IncomplException e) {    		
+    	} catch (IncomplException e) {
     		System.out.println("Could not determine main program expression type due to errors detected before type checking.");
     	} catch (TypeException e) {
-    		System.out.println("Type checking error in main program expression: "+e.text); 
-    	}       	
+    		System.out.println("Type checking error in main program expression: "+e.text);
+    	}
     	System.out.println("You had "+FOOLlib.typeErrors+" type checking errors.\n");
 
     	int frontEndErrors = lexer.lexicalErrors+parser.getNumberOfSyntaxErrors()+symtableVisitor.stErrors+FOOLlib.typeErrors;
 		System.out.println("You had a total of "+frontEndErrors+" front-end errors.\n");
-		
-		if ( frontEndErrors > 0) System.exit(1);   
+
+		if ( frontEndErrors > 0) System.exit(1);
 
     	System.out.println("Generating code.");
-    	String code = new CodeGenerationASTVisitor().visit(ast);        
-    	BufferedWriter out = new BufferedWriter(new FileWriter(fileName+".asm")); 
+    	String code = new CodeGenerationASTVisitor().visit(ast);
+    	BufferedWriter out = new BufferedWriter(new FileWriter(fileName+".asm"));
     	out.write(code);
-    	out.close(); 
-    	System.out.println("");
+    	out.close();
+    	System.out.println();
 
     	System.out.println("Assembling generated code.");
     	CharStream charsASM = CharStreams.fromFileName(fileName+".asm");
